fba3644c5666a306271a4f292bda0a593ccbafb0
achelabov <ventux228@mail.ru>
2020-09-23 20:50:10 +0300

    homework

--- 1_homework/5_solution.cpp
+++ 1_homework/5_solution.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <math.h>
-int count(int n)
+
+int count_of_divisors(int n)
 {
-	int c = 0;
-	for(int i = 1; i <= n; ++i)
-	{
-		if(n % i == 0)
-		{
-			c++;
-		}
-	}
-	return c;
+    int res = 0;
+
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0)
+            res += 2 - (i *i == n);
+
+    return res + 2 - (n == 1);
 }
 
 int main()
 {
-	int n;
-	std::cin >> n;
-	std::cout << count(n) << std::endl;
-	return EXIT_SUCCESS;
+        int num;
+        std::cin >> num;
+
+        std::cout << count_of_divisors(num) << std::endl;
+        return EXIT_SUCCESS;
 }
+ 
