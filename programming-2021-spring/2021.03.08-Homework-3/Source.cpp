027b1a2621960d966cce13c2c9aa3d0cb930781f
achelabov <ventux228@mail.ru>
2021-03-09 16:04:40 +0300

    homework

--- programming-2021-spring/2021.03.08-Homework-3/Source.cpp
+++ programming-2021-spring/2021.03.08-Homework-3/Source.cpp
@@ -12,12 +12,42 @@ bool isDigit(char c)
     return c >= '0' && c <= '9';
 }
 
-bool isNatural(string str, int& index)
+bool isNatural(std::string str, int& index)
 {
 	if (isDigit(str[index]))
 	{
 		while (isDigit(str[++index]));
 
+		return true;
+	}
+	return false;
+}
+
+bool isOrder(std::string str, int& index)
+{
+    if (str[index++] == 'E')
+    {
+        if (isNatural(str, index) || isSign(str[index++]) && isNatural(str, index))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isMantissa(std::string str, int& index)
+{
+    if (str[index++] == '.' && isNatural(str, index))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isReal(std::string str, int& index)
+{
+    if (isMantissa(str, index) && isOrder(str, index) || isSign(str[index++]) && isMantissa(str, index) && isOrder(str, index)) 
+    {
 		return true;
 	}
 	return false;
@@ -33,6 +63,16 @@ int main()
         getline(fin, str);
     }
     fin.close();
+    
+    int index = 0;
+    if (isReal(str, index))
+    {
+        std::cout << "=)" << std::endl;
+    }
+    else
+    {
+        std::cout << "=(" << std::endl;
+    }
 
     return 0;
 }
