913c7059921067b46ae93e2a0a20d1c09e6e39a7
achelabov <ventux228@mail.ru>
2021-03-20 23:34:17 +0300

    homework

--- programming-2021-spring/2021.03.20-Homework-4/Sourse.cpp
+++ programming-2021-spring/2021.03.20-Homework-4/Sourse.cpp
@@ -31,19 +31,18 @@ void printCount(int a, int b)
 
 void bubbleSort(int* arr, int length, int& comparisonBubble, int& permutationBubble)
 {
-	for (int i = 0; i < length - 1; ++i)
-	{
-		for (int j = 0; j < length - i - 1; ++j)
+    for (int i = 0; i < length - 1; ++i)
+    {
+        for (int j = 0; j < length - i - 1; ++j)
         {
             ++comparisonBubble;
-			if (arr[j] > arr[j + 1])
-			{
-			
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
                 ++permutationBubble;
             }
-		}
-	}
+        }
+    }
 }
 
 void insertionSort(int* a, int len, int& comparisonInsertion, int& permutationInsertion)
