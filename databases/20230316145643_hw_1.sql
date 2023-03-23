67072a2b31240d9d977d2106d11ee9a31c96539d
achelabov <ventux228@mail.ru>
2023-03-23 13:12:10 +0300

    hw 1

--- databases/20230316145643_hw_1.sql
+++ databases/20230316145643_hw_1.sql
@@ -6,10 +6,10 @@ CREATE TYPE vaccinations_type AS ENUM ('fury', 'distemper');
 
 CREATE TABLE vaccinations 
 (
-	pet_id			INTEGER			  NOT NULL,
-	vaccination		vaccinations_type NOT NULL,
-	date			TIMESTAMP		  NOT NULL, 
-	document_base64 TEXT			  NOT NULL,
+	pet_id           INTEGER           NOT NULL,
+	vaccination      vaccinations_type NOT NULL,
+	date             TIMESTAMP         NOT NULL, 
+	documents_base64 TEXT[]            NOT NULL,
 	CONSTRAINT pet_fk 
         FOREIGN KEY (pet_id)
             REFERENCES Pet (Pet_ID)
